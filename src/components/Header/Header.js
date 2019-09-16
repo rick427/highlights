850479bb6049ca304335c24dd2208cc050e7abcb
rick427 <rchardnjoku@yahoo.com>
2019-09-16 10:51:58 +0100

    setting up header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+import {Link} from 'react-router-dom';
+
+//@ :: STYLES
+const overrides = {
+    backgroundColor: '#98c5e9',
+    boxShadow: 'none',
+    padding: '10px 0',
+    borderBottom: '2px solid #00285e'
+}
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <AppBar position="fixed" style={overrides}>
+          <ToolBar style={{display: 'flex'}}>
+              <div style={{flexGrow: 1}}>
+                  <div className="header-logo">
+                      LOGO
+                  </div>
+              </div>
+
+              <Link to="/the_team">
+                  <Button color="inherit">The team</Button>
+              </Link>
+              <Link to="/the_matches">
+                  <Button color="inherit">Matches</Button>
+              </Link>
+              
+          </ToolBar>
+        </AppBar>
     )
 }
 
